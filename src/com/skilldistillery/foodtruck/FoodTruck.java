c462edb5eab9c89e67f8f0ec084b9fcfb9992e12
Ebony Johnson <ejohnson23451@gmail.com>
2021-10-15 19:22:23 -0400

    initial commit

---
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.foodtruck;
+
+public class FoodTruck {
+//this FoodTruck class has data
+	//including the id, which
+	//will be generated in a constructor from a static field
+	//that is incremented as each truck is created.
+}
---
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.foodtruck;
+
+public class FoodTruckApp {
+        //Scanner, menu (prompting user for input)
+	public static void main(String[] args) {
+//user story #2
+	}
+
+}
